6f340eb667719ef0951257a84da337f0085fc606
Antonio Ruben Paitno <antonio.patino003@gmail.com>
2021-11-03 19:51:51 -0500

    commit tree 33f00a18eaa014c4c98054bc4ea50a68c2196630

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Your code here
+function saturdayFun(activity = "roller-skate"){
+return (`This Saturday, I want to ${activity}!`)
+saturdayFun('bathe my dog')
+}
+
+function mondayWork(work = 'go to the office'){
+return (`This Monday, I will ${work}.`)
+mondayWork('work from home')
+}
+
+function wrapAdjective(){
+
+}
+
+function wrapAdjective (qwer = "*" ){
+    return function(asdf = "special" ){
+return `You are ${qwer}${asdf}${qwer}!`
+    }
+}
